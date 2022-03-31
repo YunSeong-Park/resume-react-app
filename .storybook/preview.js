f390f2bb6eb8fc2858e43bd5c522152b9dfb3d46
YunSeong Park <qkrdbstjd95@naver.com>
2022-03-31 20:07:35 +0900

    [New Feature] storybook emotion setting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import { ThemeProvider } from "@emotion/react";
+import { defaultTheme } from "../src/style";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +9,12 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story /> // Canvas 영역을 뜻함
+    </ThemeProvider>
+  ),
+];
