044b34821e63a58c46fe6b3718d1af4c232317b1
YunSeong Park <qkrdbstjd95@naver.com>
2022-04-01 23:45:47 +0900

    Revert "[New Feature] storybook emotion setting"
    This reverts commit f390f2bb6eb8fc2858e43bd5c522152b9dfb3d46.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,3 @@
-import { ThemeProvider } from "@emotion/react";
-import { defaultTheme } from "../src/style";
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -9,12 +6,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-};
-
-export const decorators = [
-  (Story) => (
-    <ThemeProvider theme={theme}>
-      <Story /> // Canvas 영역을 뜻함
-    </ThemeProvider>
-  ),
-];
+}
