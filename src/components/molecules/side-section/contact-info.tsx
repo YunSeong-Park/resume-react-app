a3bf482bb6bce7a914d0a63ac57552701cc31694
YunSeong Park <qkrdbstjd95@naver.com>
2022-04-09 12:51:48 +0900

    [New Feature] contact-info 컴포넌트 구현

---
+++ src/components/molecules/side-section/contact-info.tsx
@@ -0,0 +1,34 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx, Theme } from "@emotion/react";
+import { memo } from "react";
+import FaIcon, { Icon } from "../../atoms/icon/faIcon";
+import Title from "../../atoms/title/title";
+import SideSection from "./side-section";
+
+type Info = { icon: Icon; descript: string };
+
+interface ContactInfoProps {
+  infos: Info[];
+}
+
+const ContactInfo: React.FC<ContactInfoProps> = ({ infos }) => {
+  return (
+    <SideSection title="contact info">
+      <ul>
+        {infos.map((info) => (
+          <InfoItem info={info} />
+        ))}
+      </ul>
+    </SideSection>
+  );
+};
+
+export default memo(ContactInfo);
+
+const InfoItem: React.FC<{ info: Info }> = ({ info }) => {
+  return (
+    <li>
+      <FaIcon icon={info.icon} /> <span>{info.descript}</span>
+    </li>
+  );
+};
